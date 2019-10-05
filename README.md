45df88ba7367e6a79e6e0134b67cf5407a41aef9
denmind <fcaboyo@gmail.com>
2019-10-05 14:38:35 +0800

    October 4, 2019
    Stringify

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Examples_CS
-C# Examples for random problems
+C# Examples for problems I read
+
+[STRINGIFY]
+	Function will accept a number from 1 to 999 and convert it to a word number
+	Examples:
+	1 will output "one"
+	24 will output "twenty four"
+	385 will output "three hundred eight five"
+
+[FIZZBUZZ]
+	Function will print equivalent values for numbers 1 to 100 under special conditions:
+		~Multiples of 4 or numbers that contain number 4 will display "Fizz"
+		~Multiples of 5 or numbers that contain number 5 will display "Buzz"
+		~When both conditions of "Fizz" and "Buzz" are met display "FizzBuzz" instead
+	Examples:
+	1	1
+	3	3
+	4	Fizz
+	5	Buzz
+	20	FizzBuzz
+	41	Fizz
+	58	Buzz
+	100 FizzBuzz
---
+++ Stringify.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace Example_1
+{
+    internal class Stringify
+    {
+        public void Demonstrate()
+        {
+            string input = "";
+
+            Console.WriteLine("#1 Stringify demo: ");
+            Console.WriteLine("Function will accept a number from 1 to 999 and convert it to a word number\n");
+
+            while (input != "EXIT")
+            {
+                int number;
+
+                Console.Write("Enter number [1-999]:\t");
+
+                input = Console.ReadLine();
+                number = int.Parse(input);
+                input = Wordify(number);
+
+                Console.WriteLine("Equivalent of {0}:\t{1}\n", number, input);
+            }
+        }
+
+        private string Wordify(int num)
+        {
+            string value = "N/A";
+
+            if (Between(num, 1, 9, true))
+            {
+                //Ones place
+                value = Ones(num);
+            }
+            else if (Between(num, 10, 19, true))
+            {
+                //Special case
+                value = Special(num);
+            }
+            else if (Between(num, 20, 99, true))
+            {
+                int ones = num % 10,
+                    tens = num / 10;
+
+                //Tenths place
+                value = Tenths(tens);
+
+                //Ones place
+                value += (ones == 0) ? Special(tens) : Ones(ones);
+            }
+            else if (Between(num, 100, 999, true))
+            {
+                int hundredths = num / 100,
+                    tens = (num / 10) % 10,
+                    ones = num % 10;
+
+                //Hundredths place
+                value = Hundredths(hundredths);
+
+                //Tenths place
+                if (tens != 0)
+                {
+                    value += Tenths(tens);
+                }
+
+                //Ones place
+                value += (ones == 0) ? Special(tens) : Ones(ones);
+            }
+
+            return value;
+        }
+
+        private bool Between(int number, int left, int right, bool inclusive)
+        {
+            bool value;
+
+            if (inclusive)
+            {
+                value = (number >= left && number <= right);
+            }
+            else
+            {
+                value = (number > left && number < right);
+            }
+
+            return value;
+        }
+
+        private string Special(int num)
+        {
+            string value = "";
+
+            switch (num)
+            {
+                case 10: value = "ten"; break;
+                case 11: value = "eleven"; break;
+                case 12: value = "twelve"; break;
+                case 13: value = "thirteen"; break;
+                case 14: value = "fourteen"; break;
+                case 15: value = "fifteen"; break;
+                case 16: value = "sixteen"; break;
+                case 17: value = "seventeen"; break;
+                case 18: value = "eighteen"; break;
+                case 19: value = "nineteen"; break;
+            }
+
+            return value;
+        }
+        private string Hundredths(int num)
+        {
+            string value = "";
+
+            //Get equivalent
+            value = Ones(num);
+
+            //Add word hundred
+            value += " hundred";
+
+            //Add space
+            value += " ";
+
+            return value;
+        }
+        private string Tenths(int num)
+        {
+            string value = "";
+
+            switch (num)
+            {
+                case 2: value = "twenty"; break;
+                case 3: value = "thirty"; break;
+                case 4: value = "fourty"; break;
+                case 5: value = "fifty"; break;
+                case 6: value = "sixty"; break;
+                case 7: value = "seventy"; break;
+                case 8: value = "eighty"; break;
+                case 9: value = "ninety"; break;
+            }
+
+            //Add space
+            value += " ";
+
+            return value;
+        }
+        private string Ones(int num)
+        {
+            string value = "";
+
+            switch (num)
+            {
+                case 1: value = "one"; break;
+                case 2: value = "two"; break;
+                case 3: value = "three"; break;
+                case 4: value = "four"; break;
+                case 5: value = "five"; break;
+                case 6: value = "six"; break;
+                case 7: value = "seven"; break;
+                case 8: value = "eight"; break;
+                case 9: value = "nine"; break;
+            }
+
+            return value;
+        }
+    }
+}
